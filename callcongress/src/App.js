e9e8b8d6a81bf9928e63009bccff2724b2e4a143
J Silverstein <jess.pebble@gmail.com>
2017-01-12 19:52:42 -0500

    logging in with github ACHIEVED

--- callcongress/src/App.js
+++ callcongress/src/App.js
@@ -8,17 +8,51 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+    //* binds *//
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+    this.authenticate = this.authenticate.bind(this);
+
+    //* state *//
+    this.state = {
+      uid: null,
+    }
   }
 
   componentDidMount() {
-    firebase.auth();
+
+  }
+
+  authenticate() {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then((res) => {
+        if (res.credential) {
+          var token = res.credential.accessToken;
+        }
+        var user = res.user;
+        this.setState({uid: user})
+      }).catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  renderLogin () {
+    if (this.state.uid === null) {
+      return (
+        <nav className="login">
+          <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
+        </nav>
+        )
+    } else {
+      return (<p>You are logged in!</p>)
+    }
   }
 
   render() {
     return (
       <div className="App">
-
+          {this.renderLogin()}
       </div>
     );
   }
