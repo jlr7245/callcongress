254641296ad0425c59b079d09e115506cf7de03e
J Silverstein <jess.pebble@gmail.com>
2017-01-12 23:27:42 -0500

    authentication....................... yikes

--- callcongress/src/App.js
+++ callcongress/src/App.js
@@ -5,13 +5,18 @@ import firebase from 'firebase';
 import firebaseSpecs from './components/key.js';
 import './App.css';
 
+//* setting API variables *//
+const fbaseKey = firebaseSpecs.apiKey;
+const fbaseUrl = firebaseSpecs.databaseURL;
+
 class App extends Component {
   constructor() {
     super();
     //* binds *//
     this.componentDidMount = this.componentDidMount.bind(this);
-    this.renderLogin = this.renderLogin.bind(this);
+    //this.renderLogin = this.renderLogin.bind(this);
     this.authenticate = this.authenticate.bind(this);
+    this.testPost = this.testPost.bind(this);
 
     //* state *//
     this.state = {
@@ -29,22 +34,38 @@ class App extends Component {
       .then((res) => {
         if (res.credential) {
           var token = res.credential.accessToken;
+        } else {
+          //this.createUser(res.user.uid);
         }
-        var user = res.user;
-        this.setState({uid: user})
+        const user = axios.create({
+          baseURL: fbaseUrl,
+          auth: token
+        });
+        this.setState({uid: res.user.uid, useraxios: user});
       }).catch((err) => {
         console.log(err.message);
       });
   }
 
+  testPost() {
+    this.state.useraxios.post('/users.json', { id: this.state.uid, joinedOn: moment() })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.message));
+  }
+
+/*  createUser(id) {
+    axios.post(`${fbaseURL}/users/${id}.json?apiKey=${}`)
+  }*/
+
   renderLogin () {
     if (this.state.uid === null) {
       return (
         <nav className="login">
-          <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
+          <button className="github" onClick={() => this.authenticate()}>Log In with Github</button>
         </nav>
         )
     } else {
+      console.log(this.state.uid);
       return (<p>You are logged in!</p>)
     }
   }
