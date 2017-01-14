d4427ac5ebf1c11325d2524325c6693cc123a896
J Silverstein <jess.pebble@gmail.com>
2017-01-13 20:19:13 -0500

    gitignore

--- callcongress/src/components/keys/sunlight.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const sunlightAXIOS = axios.create({
-  baseURL: `https://congress.api.sunlightfoundation.com/`,
-  headers: {'Content-Type': 'application/json'}
-})
-
-export default sunlightAXIOS;
