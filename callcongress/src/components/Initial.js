24baa382de0691defc7abc2b1a0d21d01f78ddcd
J Silverstein <jess.pebble@gmail.com>
2017-01-17 00:10:23 -0500

    legislators RENDER on the pagegit add .git add .

--- callcongress/src/components/Initial.js
+++ callcongress/src/components/Initial.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 class Initial extends React.Component {
+  constructor() {
+    super();
+    this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this);
+    this.inspirationalQuotes=[
+    'Decisions are made by those who show up.',
+    'The voice of the people cannot be silenced.',
+    'We must do better and we will do better.',
+    'There is work to be done.',
+    'Thoughtful & committed citizens can change the world.'
+    ]
+  }
 
+  shouldComponentUpdate(nextProps) {
+    return !(this.props === nextProps); /// doesnt have the desired effect :(
+  }
+
+  componentDidUpdate() {
+    console.log('update');
+  }
 
   render() {
     return (
       <div className="init">
         <div className="left">
-          <h1>Decisions are made by those who show up.</h1>
+          <h1>{this.inspirationalQuotes[Math.floor(Math.random() * this.inspirationalQuotes.length)]}</h1>
         </div>
         <div className="right">
           <div className="input">
--- callcongress/src/components/Legislators.js
+++ callcongress/src/components/Legislators.js
@@ -1,5 +1,6 @@
 import allLegislators from './keys/legislators.json';
 import React from 'react';
+import moment from 'moment';
 
 class Legislators extends React.Component {
   constructor() {
@@ -14,17 +15,64 @@ class Legislators extends React.Component {
   }*/ // this would have the desired behavior of not rerendering the legislators but looking at the docs it looks like it doesn't make a difference
 
   renderLegislators() {
-
+    let legislatorList = allLegislators.results
+      .filter((i) => this.props.legislatorsToLoad.indexOf(i.bioguide_id) !== -1);
+    let formattedLegs = legislatorList.map((leg) => {
+      return (
+        <li className={leg.party}>
+         <div className='leghead'>
+           <img src={`https://theunitedstates.io/images/congress/225x275/${leg.bioguide_id}.jpg`} />
+           <div className='headinfo'>
+             <i className={`fa fa-circle-o fa-2x ${leg.party}`}></i>
+             <h3>{leg.first_name} {leg.last_name}</h3>
+             <span className='represents'>{leg.state_name}</span>
+             <p className='currentterm'>Started {moment(leg.term_start).format('MMM d, YYYY')}</p>
+             <p className='currentterm'>Ends {moment(leg.term_end).format('MMM d, YYYY')}</p>
+           </div>
+         </div>
+         <div className='legbody'>
+           <div className='legadd'>
+            <i className='fa fa-map-marker fa-3x'></i>
+             <p>{leg.office}<br/>
+               Washington DC 20510</p>
+           </div>
+           <div className='legphn'>
+             <i className="fa fa-phone fa-3x"></i>
+             <h3>{leg.phone}</h3>
+           </div>
+         </div>
+       </li>
+      )
+    });
+    return formattedLegs;
   }
 
   render() {
-    let legislatorList = allLegislators.results
-      .filter((i) => this.props.legislatorsToLoad.indexOf(i.bioguide_id) !== -1);
-    console.log(legislatorList);
     return (
-        <p>Your legislators go HERE!</p>
+        <div className='leglist'>
+          <ul>
+            {this.renderLegislators()}
+          </ul>
+        </div>
       )
   }
 }
 
 export default Legislators;
+
+
+/*
+<div className='legsoc'>
+             {if (leg.hasOwnProperty('website') && leg.website !== null) return (<a href={leg.website} target='_blank'>
+               <i className='fa fa-link fa-2x fa-fw'></i>
+             </a>)}
+            {if (leg.hasOwnProperty('twitter_id') && leg.twitter_id !== null) return (<a href={`https://www.twitter.com/${leg.twitter_id}`} target='_blank'>
+               <i className='fa fa-2x fa-fw fa-twitter'></i>
+             </a>) }
+             {if (leg.hasOwnProperty('facebook_id') && leg.facebook_id !== null) return (<a href={`https://www.facebook.com/${leg.facebook_id}`} target='_blank'>
+               <i className='fa fa-2x fa-fw fa-facebook'></i>
+             </a>)}
+             {if (leg.hasOwnProperty('youtube_id') && leg.youtube_id !== null) return(<a href='https://www.youtube.com/user/ZeldinForCongress' target='_blank'>
+               <i className='fa fa-2x fa-fw fa-youtube'></i>
+             </a>)}
+*/ /// all of these need to be put in variables unfortunately
